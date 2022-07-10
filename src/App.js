df22fa33b8d8acfcc2046ee1b6cd428074762762
kimje1619 <kimje1619@naver.com>
2022-07-10 16:57:31 +0900

    update : [React] 6. event

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+
+
+function Article(props){
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
 function Header(props){
-  console.log('props', props, props.title);
+  // 받아오는 속성
+  // 여기서 사용한 태그들은 순수한 HTML태그가 아님. 유사 HTML임, 그래서 여기서 사용하는 문법은 똑같지 않음.
   return <header>
-    <h1><a href="/">{props.title}</a></h1>
+    <h1><a href="/" onClick={(event)=>{
+      event.preventDefault();
+      // 속성인 함수를 호출하는 것
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
   </header>
 }
 function Nav(props){
+  // target은 event를 유발시킨 태그를 가리킨다. (a 태그)
+  // a태그의 id값을 가져온다 target.id
   const lis = []
   for(let i=0; i<props.topics.length; i++){
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{t.title}</a>
+    </li>)
   }
   return <nav>
     <ol>
       {lis}
     </ol>
   </nav>
-}
-function Article(props){
-  return <article>
-    <h2>{props.title}</h2>
-    {props.body}
-  </article>
 }
 function App() {
   const topics = [
@@ -32,8 +46,12 @@ function App() {
   ] 
   return (
     <div>
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav>
+      <Header title="WEB" onChangeMode={()=>{
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
       <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
