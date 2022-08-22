11cd56167e9a800557e7c5ac8a64b360adb2797e
hedy.kim <hedy.kim@kakaocorp.com>
2022-08-22 10:53:00 +0900

    update : (2) conflict 예시파일 업로드

---
+++ src/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Main = (props) => {
+	return (
+		<>
+			<h3>메인화면</h3>
+		</>
+	);
+};
+
+export default Main;
---
+++ src/Product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Product = (props) => {
+    return (
+        <>
+            <h3>상품</h3>
+        </>
+    );
+}
+
+export default Product;
